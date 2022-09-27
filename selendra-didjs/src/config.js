14cd4bfc38d9bd98e3cf1f253e6188e78fbccecf
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-09-27 11:21:18 +0700

    restructure

--- selendra-didjs/src/config.js
+++
@@ -1,9 +0,0 @@
-import Identity from "./static/Identity.json";
-const config = {
-  providerAddress: "wss://rpc-testnet.selendra.org",
-  contractAddress: "0xaA3cE26BC4742a03a8cD4c1Ba152c1687263481E",
-  abi: Identity.abi,
-  api: "https://gateway.kumandra.org",
-};
-
-export default config;
